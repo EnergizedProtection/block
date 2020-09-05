b32f9410227e90f4fac94dc63e12f12d15740684
Imre Kristoffer Eilertsen <imreeil42@gmail.com>
2020-09-05 02:37:24 +0200

    Update build.sh

--- extensions/xtreme/build.sh
+++ extensions/xtreme/build.sh
@@ -104,10 +104,10 @@ sed '/#/d' -i $file
 sed '/.blogspot./d;/.wixsite./d;/.wordpress./d' -i $file
 # Remove Blank/Empty Lines
 sed '/^$/d' -i $file
-# Attempts to remove entries that end with backslashes
-sed '/\$/d' -i $file
+# Attempts to remove entries with backslashes
+sed '/\\/d' -i $file
 # Attempts to remove entries that end with dots
-sed '/.$/d' -i $file
+sed '/\.$/d' -i $file
 # Removes Whitespace
 cat $file | tr -d '\r' >> $temp
 # Sort, Remove Duplicate and Write
